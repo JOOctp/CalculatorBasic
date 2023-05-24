b24a9765549dedb761593339206a3b515a418a29
Jevon Putra <jevon.octavian@gocement.com>
2023-05-24 23:39:49 +0700

    Initial Commit

---
+++ BasicCalculator/BasicCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicCalculatorApp.swift
+//  BasicCalculator
+//
+//  Created by Gocement on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BasicCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
